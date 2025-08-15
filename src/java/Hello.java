1d445e4dc44482012222853788116a731bccb8e0
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-15 12:07:46 +0800

    feat: initial commit for src/java/Hello.java

---
+++ src/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(hello("Welt"));
+    }
+    public static String hello(String name) {
+        return "Hallo, " + name + "!";
+    }
+}
